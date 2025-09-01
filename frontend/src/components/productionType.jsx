83e24c5108e29e1c658d705376650af2180d4452
nidhi <dattainidhi37@gmail.com>
2025-09-01 17:13:27 +0530

    producttype

---
+++ frontend/src/components/productionType.jsx
@@ -0,0 +1,31 @@
+export default function ProductionType({ producttype, setproducttype, rate, setRate }) {
+  return (
+    <div>
+      <label>
+        <input
+          type="radio"
+          checked={producttype === "inhouse"}
+          onChange={() => setproducttype("inhouse")}
+        /> Inhouse
+      </label>
+      <label>
+        <input
+          type="radio"
+          checked={producttype === "jobwork"}
+          onChange={() => setproducttype("jobwork")}
+        /> Jobwork
+      </label>
+
+      {producttype === "jobwork" && (
+        <div>
+          Job rate/unit:{" "}
+          <input
+            type="number"
+            value={rate}
+            onChange={(e) => setRate(e.target.value)}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
